14f6d95a2ee9a5731460fd8fbecdc0eb6ba5d5cc
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-03-30 20:10:57 +0200

    Estoy creado el javaScript con escuchadores de eventos sobre los elementos href para implementar hover

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
-const name = "World";
-console.log(`Hello ${name}`);
+const name = "Herramientas HTML y CSS";
+console.log(`Bienvenido a la PEC 1 de ${name}`);
+
+
+const element = document.querySelector("#enlace");
+
+element.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+  element.classList.add("hoverIn");
+  element.classList.remove("hoverOut");
+});
+
+element.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+  element.classList.add("hoverOut");
+  element.classList.remove("hoverIn");
+});
