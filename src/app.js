77496e428b4be8beb94dbaf479d373ecfc54416e
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-04-03 11:45:43 +0200

    He modificado el código en app.js

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const elementList = document.querySelectorAll ("a");
 let elementsArr = Array.from(elementList);
 
 elementsArr.forEach(aElement => aElement.addEventListener("mouseover", event => {
-  console.log("Mouse in");
+  
   aElement.classList.add("hoverIn");
   aElement.classList.remove("hoverOut");
 }))
 
 elementsArr.forEach(aElement => aElement.addEventListener("mouseout", event => {
-  console.log("Mouse out");
+  
   aElement.classList.add("hoverOut");
   aElement.classList.remove("hoverIn");
 }))
