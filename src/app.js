b6f05ad1e47dccb33eef08c44b08ae18513c4cf1
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-04-06 20:15:14 +0200

    Modifico app.js para intentar usar el componente Lazy Load Image

--- src/app.js
+++ src/app.js
@@ -16,3 +16,19 @@ elementsArr.forEach(aElement => aElement.addEventListener("mouseout", event => {
   aElement.classList.add("hoverOut");
   aElement.classList.remove("hoverIn");
 }))
+
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+const MyImage = ({ image }) => (
+  <div>
+    <LazyLoadImage
+      alt={image.alt}
+      height={image.height}
+      src={image.src} // use normal <img> attributes as props
+      width={image.width} />
+    <span>{image.caption}</span>
+  </div>
+);
+
+export default MyImage;
