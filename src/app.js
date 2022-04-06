134bfcb7aee74f09ddc340d852a6f4fb6f5a94de
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-04-06 21:15:43 +0200

    Comento app.js ya que genera un error al compilar

--- src/app.js
+++ src/app.js
@@ -20,15 +20,15 @@ elementsArr.forEach(aElement => aElement.addEventListener("mouseout", event => {
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const MyImage = ({ image }) => (
+/* const MyImage = ({ image }) => (
   <div>
     <LazyLoadImage
       alt={image.alt}
       height={image.height}
-      src={image.src} // use normal <img> attributes as props
+      src={image.src} 
       width={image.width} />
     <span>{image.caption}</span>
   </div>
-);
+); */
 
 export default MyImage;
