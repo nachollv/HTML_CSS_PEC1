45bcbf3c0b259a6f2570d8416d0caf35bac9ec86
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-04-11 20:03:41 +0200

    He elimninado la referencia al componente en package.json, en modficado app.js e index.html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import React from "react";
-import { render } from "react-dom";
-import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
-
-const BasicRows = () => <Gallery photos={photos} />;
-render(<BasicRows />, document.getElementById("app"));
-
 const name = "Herramientas HTML y CSS";
 console.log(`Bienvenido a la PEC 1 de ${name}`);
 
@@ -24,24 +16,3 @@ elementsArr.forEach(aElement => aElement.addEventListener("mouseout", event => {
   aElement.classList.add("hoverOut");
   aElement.classList.remove("hoverIn");
 }))
-
-var items = [
-  {
-      src: 'path/to/image1.jpg', // path to image
-      title: 'Image Caption 1' // If you skip it, there will display the original image name(image1)
-  },
-  {
-      src: 'path/to/image2.jpg',
-      title: 'Image Caption 2'
-  }
-];
-
-// define options (if needed)
-var options = {
-  // optionName: 'option value'
-  // for example:
-  index: 0 // this option means you will start at first image
-};
-
-// Initialize the plugin
-var viewer = new PhotoViewer(items, options);
